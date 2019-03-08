ea8b38ea2a3be6d52e13d8bb70b7c26a4134990a
caohao0730 <caohao0730@163.com>
2019-03-08 17:10:29 +0800

    Update third.php

--- application/config/third.php
+++ application/config/third.php
@@ -6,7 +6,7 @@
  * Time: 下午5:07
  */
 
-$config['access_token']['wx38286d9af38babe5']['app_id'] = 'wx38286d9af38babe5'; //大保健
-$config['access_token']['wx38286d9af38babe5']['secret'] = '54f747daaf8062020d3d55f047cb5dd2';
+$config['access_token']['cxcvxvxcv']['app_id'] = 'xcvxxccccccccccccvxdf';
+$config['access_token']['ascdasdfasdf']['secret'] = 'asdfasdcasdc';
 
 return $config;
