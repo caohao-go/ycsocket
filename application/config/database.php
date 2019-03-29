99437c422d10a46b375a9e18bccd4f771243aa7b
caohao0730 <caohao0730@163.com>
2019-03-29 23:16:37 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -51,7 +51,6 @@ $util_db_config['default']['dbname']   = 'user';
 $util_db_config['default']['pconnect'] = FALSE;
 $util_db_config['default']['char_set'] = 'utf8';
 $util_db_config['default']['dbcollat'] = 'utf8_general_ci';
-$util_db_config['default']['autoinit'] = FALSE;
 
 $util_db_config['shine_light']['host']     = '127.0.0.1';
 $util_db_config['shine_light']['username'] = 'root';
@@ -60,7 +59,6 @@ $util_db_config['shine_light']['dbname']   = 'shine_light';
 $util_db_config['shine_light']['pconnect'] = FALSE;
 $util_db_config['shine_light']['char_set'] = 'utf8';
 $util_db_config['shine_light']['dbcollat'] = 'utf8_general_ci';
-$util_db_config['shine_light']['autoinit'] = FALSE;
 $util_db_config['shine_light']['pool_size'] = 15;
 
 /* End of file database.php */
