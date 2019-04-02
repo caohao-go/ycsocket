982059510501e407fff83d6b937df54da0294014
php <caohao0730@163.com>
2019-04-02 01:41:43 +0000

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -4,7 +4,6 @@ $util_db_config['default']['host']     = '127.0.0.1';
 $util_db_config['default']['username'] = 'root';
 $util_db_config['default']['password'] = '123123';
 $util_db_config['default']['dbname']   = 'user';
-$util_db_config['default']['pconnect'] = FALSE;
 $util_db_config['default']['char_set'] = 'utf8';
 $util_db_config['default']['dbcollat'] = 'utf8_general_ci';
 
@@ -12,7 +11,6 @@ $util_db_config['shine_light']['host']     = '127.0.0.1';
 $util_db_config['shine_light']['username'] = 'root';
 $util_db_config['shine_light']['password'] = '123123';
 $util_db_config['shine_light']['dbname']   = 'shine_light';
-$util_db_config['shine_light']['pconnect'] = FALSE;
 $util_db_config['shine_light']['char_set'] = 'utf8';
 $util_db_config['shine_light']['dbcollat'] = 'utf8_general_ci';
 $util_db_config['shine_light']['pool_size'] = 15;
