4c51b7728677fce63c198376f88c4eb97845629b
caohao0730 <caohao0730@163.com>
2019-08-19 10:09:37 +0800

    Update constants.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,8 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*********************************** 常量定义*****************************************/
-define("WEIXIN_OPEN_ID_URL", "https://api.weixin.qq.com/sns/jscode2session?");
-define("WEIXIN_SNSAPI_USER_INFO_URL", "https://api.weixin.qq.com/sns/userinfo?");
-
 //排行榜平均默认每页多少条数据
 define("RANK_PER_PAGE", 10);
